44c64edf481f074de2d57885ed1a58265dd30378
ditchk <blackdollar254@gmail.com>
2023-11-22 17:33:41 +0300

    my first commit

--- src/components/shared/Socials.tsx
+++ src/components/shared/Socials.tsx
@@ -3,12 +3,16 @@ import { RiInstagramFill } from "react-icons/ri";
 import { ImFacebook2 } from "react-icons/im";
 import { FaXTwitter } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 
 const Socials = () => {
   return (
     <div className="flex flex-row gap-3 p-3">
-      <FaSlack className="social_icons" />
+      <Link to={'https://benkigochi5.slack.com/archives/C04SPHHHN4F'}>
+        <FaSlack className="social_icons" />
+      </Link>
+      
       <RiInstagramFill className="social_icons" />
       <ImFacebook2 className="social_icons" />
       <FaXTwitter className="social_icons" />
