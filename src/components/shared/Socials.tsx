d8d206796123a14b19c4a44570a577cb45c960d2
ditchk <blackdollar254@gmail.com>
2023-11-22 19:36:27 +0300

    my first commit

--- src/components/shared/Socials.tsx
+++ src/components/shared/Socials.tsx
@@ -1,8 +1,9 @@
 import { FaSlack } from "react-icons/fa";
 import { RiInstagramFill } from "react-icons/ri";
 import { ImFacebook2 } from "react-icons/im";
-import { FaXTwitter } from "react-icons/fa6";
+import { FaXTwitter,FaLinkedin } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
+
 import { Link } from "react-router-dom";
 
 
@@ -12,11 +13,21 @@ const Socials = () => {
       <Link to={'https://benkigochi5.slack.com/archives/C04SPHHHN4F'}>
         <FaSlack className="social_icons" />
       </Link>
-      
-      <RiInstagramFill className="social_icons" />
-      <ImFacebook2 className="social_icons" />
-      <FaXTwitter className="social_icons" />
-      <SiGmail className="social_icons" />
+      <Link to={}>
+        <RiInstagramFill className="social_icons" />
+      </Link>
+      <Link to={'https://www.linkedin.com/in/BensonKigoci'}>
+        <FaLinkedin className="social_icons" />
+      </Link>
+      <Link to={'/'}>
+        <ImFacebook2 className="social_icons" />
+      </Link>
+      <Link to={'https://twitter.com/BKigoci32396'}>
+        <FaXTwitter className="social_icons" />
+      </Link>
+      <Link to={'mailto:bensonkigoci@gmail.com?subject=I%20would%20like%20to%20schedule%20a%20zoom%20meeting&body=Hello%20there%20Benson%2C%20bensonkigoci@gmail.com'}>
+        <SiGmail className="social_icons" />
+      </Link>
     </div>
   )
 }
