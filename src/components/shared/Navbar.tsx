0fdd13873924c824cbd56853c460a4ba9d208c9d
ditchk <blackdollar254@gmail.com>
2023-11-22 14:13:56 +0300

    my first commit

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,10 +1,6 @@
-import  Sidebar  from './Sidebar'
 import { Button } from '../ui/button';
 import Socials from './Socials';
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
-import { sideBarLinks } from '@/constants';
-import { MyNavLink } from '@/types';
-import { NavLink, useLocation } from 'react-router-dom';
 
 
 const Navbar = () => {
