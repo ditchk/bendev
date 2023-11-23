d013b318564c9a0816486b034c8d78d9a10c16ea
ditchk <blackdollar254@gmail.com>
2023-11-23 17:20:15 +0300

    my first commit

--- src/components/shared/ProjectForm.tsx
+++ src/components/shared/ProjectForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form"
 import * as z from "zod"
 
 import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "../ui/textarea"
 import FileUploader from "./FileUploader"
