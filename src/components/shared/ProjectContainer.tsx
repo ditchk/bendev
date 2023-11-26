7e76d87a6f34e4635c15470ba824255694d48d11
ditchk <blackdollar254@gmail.com>
2023-11-26 17:43:46 +0300

    my first commit

--- src/components/shared/ProjectContainer.tsx
+++ src/components/shared/ProjectContainer.tsx
@@ -8,20 +8,20 @@ type projectCardProps = {
 const ProjectContainer = ({ project }: projectCardProps) => {
   return (
     <div className="project_container cursor-pointer">
-      <div className="flex flex-col justify-center p-2">
+      <div className="flex flex-col justify-center gap-3 p-2">
         <div className="flex flex-1 justify-start items-start p-2 gap-3 bg-transparent rounded-md">
           <img src={project.imageUrl} alt={project.imageUrl} className="h-10 w-10 object-contain rounded-xl" />
           <h1 className="primary-projecttext">{project.ProjectName}</h1>
         </div>
  
         <p className="project_paragraph">{project.projectInfo}</p>
-        <div className="max-h-[200px] overflow-hidden shadow">
+        {/* <div className="max-h-[200px] overflow-hidden rounded-2xl mx-10">
         <img 
           src={project.imageUrl} 
           alt=""
           className="object-cover" 
           />
-        </div>
+        </div> */}
           <Button className="custom_button">
             learn more
           </Button>
