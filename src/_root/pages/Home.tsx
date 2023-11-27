3c0570f05e1ab2161eac5cb6fb8444662c16c100
ditchk <blackdollar254@gmail.com>
2023-11-27 18:28:01 +0300

    Latest updates

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <div className="default_Container">
       
-      <div className="custom_container">
+      <div className="custom_container h-full">
         {/* <VideoPlayer src="https://cloud.appwrite.io/v1/storage/buckets/655777900bd1083e6876/files/6561e25fea7de27f352d/view?project=655773f801147821ba93&mode=admin" /> */}
         <Welcome />
       </div> 
