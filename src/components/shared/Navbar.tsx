0b2426bbc7a0fdfa036e4495eecfd71a987569b4
ditchk <blackdollar254@gmail.com>
2023-11-29 17:05:22 +0300

    Latest Updates

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -7,8 +7,13 @@ const Navbar = () => {
 
   return (
     <nav className="z-50 flex flex-row justify-between gap-10 w-full h-max md:bg-transparent md:backdrop-blur top-0 sticky shadow-md shadow-slate-400 py-2 px-4 bg-white md:bg-opacity-5">
-      <Link to={'/'} >
-        <img src="/assets/images/benswsp.png" alt="" width={200}/>
+      <Link 
+      to={'/'}
+      className="flex flex-row justify-center items-center gap-2" 
+      
+      >
+        <img src="/assets/images/benpwspace.png" alt="" width={50}/>
+        <h1 className="text-center text-xl font-extrabold font-serif text-transparent bg-clip-text bg-gradient-to-br from-black via-cyan-800 to-violet-700">BENSWORKSPACE</h1>
       </Link>
       <div className="hidden md:flex flex-row">
         <h1 className="text-center text-black text-xl p-3 font-serif">lets talk!</h1>
