204f65a2a93fb26e5a2dcd395809b6848f2dfe55
ditchk <blackdollar254@gmail.com>
2023-12-19 18:55:38 +0300

    Latest bensworkspace updates

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -55,7 +55,7 @@ const SigninForm = () => {
           }}
           ref={ref} 
         onSubmit={form.handleSubmit(onSubmit)} 
-        className="flex flex-col justify-start items-center h-fit p-3 md:px-16 shadow-lg py-8 shadow-cyan-950 bg-cyan-950 bg-opacity-10 w-auto space-y-3 rounded-md outline outline-1 outline-slate-200">
+        className="flex flex-col justify-start items-center h-fit px-9 md:px-16 shadow-lg py-8 shadow-cyan-950 bg-cyan-950 bg-opacity-10 w-auto space-y-3 rounded-md outline outline-1 outline-slate-200">
          <h1 className="headText no-underline">WELCOME BACK</h1>
         <FormField
           control={form.control}
