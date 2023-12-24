56ee2c4f89ab32cad3c5e0777d6c6b1b3238c694
ditchk <blackdollar254@gmail.com>
2023-12-24 07:10:22 +0300

    Minor changes

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white focus-visible:outline outline-1 outline-neutral-50 disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline outline-1 outline-neutral-50 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -10,7 +10,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[50px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-white focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 custom-scrollbar",
+          "flex min-h-[50px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 custom-scrollbar",
           className
         )}
         ref={ref}
