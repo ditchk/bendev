a9765af7b43174c33265d3235ed965d16a6aa098
ditchk <blackdollar254@gmail.com>
2023-12-26 16:56:24 +0300

    Minor changes

--- src/components/shared/ProjectLoder.tsx
+++ src/components/shared/ProjectLoder.tsx
@@ -1,16 +1,16 @@
-import { waveform } from 'ldrs'
+import { grid } from 'ldrs'
 
-waveform.register()
+grid.register()
 
 
 const ProjectLoder = () => {
   return (
     <div className='flex flex-1 justify-center items-center min-h-[500px] w-full bg-transparent z-0'>
-      <l-waveform
+      <l-grid
       size="50"
       speed="1" 
       color="#027d8b"
-    ></l-waveform>
+    ></l-grid>
   </div>
   )
 }
