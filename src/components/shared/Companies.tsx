c7821bdbefa7759f6f9d52fa6698f454564e1a90
ditchk <blackdollar254@gmail.com>
2023-12-30 23:51:44 +0300

    updated designs

--- src/components/shared/Companies.tsx
+++ src/components/shared/Companies.tsx
@@ -16,9 +16,9 @@ const Companies = () => {
   return (
     <div 
     style={{
-      transform: isInView ? "none" : "translateY(50px)",
+      transform: isInView ? "none" : "translateY(200px)",
       opacity: isInView ? 1 : 0,
-      transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.2s"
+      transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
     }}
     ref={ref}
       className="flex flex-row justify-center items-center gap-3 md:gap-10">
