5e6c5ad5d83d2f2320c04135a400a7f266cf8a7a
ditchk <blackdollar254@gmail.com>
2024-01-03 22:23:13 +0300

    updates

--- src/components/shared/Welcome.tsx
+++ src/components/shared/Welcome.tsx
@@ -21,14 +21,7 @@ const Welcome = () => {
               <div className="flex flex-col md:flex-row w-fit gap-5 md:gap-0 md:p-5 animate-in slide-in-from-up-40 duration-500 overflow-hidden">
                   <li key={link.title} >
                     <h1 className="tittl_name">{link.title}</h1>
-                      <div className="flex flex-row justify-center items-center mt-5">
-                        <h2 className="paragraph_text">{link.subTitle}</h2>
-                        <div className="flex w-20 sm:w-72  shadow-sm shadow-slate-400 justify-center bg-white h-[1px]" />
-                      <div className="flex w-[25px]  shadow-sm shadow-slate-400 md:w-[35px] justify-center rounded-full bg-gradient-to-tl from-cyan-950 to-cyan-200 outline outline-1 outline-white h-[25px]" />
-                      </div>
-                    
-                    <div className="flex flex-col">
-                    <div className="flex w-20 sm:w-72  shadow-sm shadow-slate-400 justify-center bg-white h-[1px]" />
+                    <div className="flex w-full justify-center">
                         <img 
                           src="/assets/images/homepage.png" 
                           alt="homepage-image" 
@@ -36,10 +29,16 @@ const Welcome = () => {
                           loading="lazy"
                           className="md:hidden object-contain bg-transparent h-fit rounded-e-full justify-end rounded-t-full border-b-2 overflow-hidden animate-in slide-in-from-right-24 duration-1000 border-slate-50"
                         />
-                        <div className="flex absolute w-[1px] shadow-xl shadow-slate-400 justify-center bg-white h-98" /> 
                     </div> 
+                      <div className="flex flex-row justify-center items-center mt-5">
+                        <h2 className="paragraph_text">{link.subTitle}</h2>
+                        <div className="flex w-20 sm:w-72  shadow-sm shadow-slate-400 justify-center bg-white h-[1px]" />
+                      <div className="flex w-[25px]  shadow-sm shadow-slate-400 md:w-[35px] justify-center rounded-full bg-gradient-to-tl from-cyan-950 to-cyan-200 outline outline-1 outline-white h-[20px] md:h-[25px]" />
+                      </div>
+                    
+                    
                     <div className="flex flex-row justify-center items-center mt-5">
-                      <div className="flex w-[25px]  shadow-sm shadow-slate-400 md:w-[35px] justify-center rounded-full bg-gradient-to-tl from-cyan-950 to-cyan-200 outline outline-1 outline-white h-[25px]" />
+                      <div className="flex w-[25px]  shadow-sm shadow-slate-400 md:w-[35px] justify-center rounded-full bg-gradient-to-tl from-cyan-950 to-cyan-200 outline outline-1 outline-white h-[20px] md:h-[25px]" />
                       <div className="flex w-20 sm:w-72  shadow-sm shadow-slate-400 justify-center bg-white h-[1px]" />
                       <h3 className="paragraph_text shadow-sm shadow-slate-400 ">I have aquired skills that will help your business scale up in this technology era.Using my expertise i will design stunning landing pages/website and even design your ad campaignsfor your business using the latest technology <br /> Click the button below to talk to me (❁´◡`❁)<Link className="custom_button p-2 mt-5 rounded-lg text-white font-serif flex" to={'/mycv'}>Continue reading</Link></h3>
                     </div>
