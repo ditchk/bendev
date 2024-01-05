35aa76a1922eab0c3d2da9151dc5eb185a044073
ditchk <blackdollar254@gmail.com>
2024-01-05 11:54:15 +0300

    test style

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -7,8 +7,8 @@ import Welcome from "@/components/shared/Welcome"
 const Home = () => {
   
   return (
-    <div className="default_Container h-full"> 
-      <div className="custom_Homecontainer h-full">
+    <div className="default_Container"> 
+      <div className="custom_Homecontainer">
         {/* <VideoPlayer src="https://cloud.appwrite.io/v1/storage/buckets/655777900bd1083e6876/files/6561e25fea7de27f352d/view?project=655773f801147821ba93&mode=admin" /> */}
         <Welcome />
         <Experience />
