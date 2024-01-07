f4aef7abb2bb757f6d6ce11d06919480bf9bdc18
ditchk <blackdollar254@gmail.com>
2024-01-07 13:36:03 +0300

    updated Homepage page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,3 @@
-
 import Brands from "@/components/shared/Brands"
 import Experience from "@/components/shared/Experience"
 import Services from "@/components/shared/Services"
@@ -9,7 +8,6 @@ const Home = () => {
   return (
     <div className="default_Container"> 
       <div className="custom_Homecontainer">
-      <iframe src="https://geo.dailymotion.com/player/xndrv.html?video=" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen width="640" height="360"></iframe>
         <Welcome />
         <Experience />
         <Brands />
