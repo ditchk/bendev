0722d219e0f932c4bc66970a717435d7a0c8eccb
ditchk <blackdollar254@gmail.com>
2024-01-08 18:38:41 +0300

    updated styles page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -4,7 +4,6 @@ import Services from "@/components/shared/Services"
 import Welcome from "@/components/shared/Welcome"
 
 const Home = () => {
-  
   return (
     <div className="default_Container"> 
       <div className="custom_Homecontainer">
