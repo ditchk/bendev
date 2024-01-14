341eceb857ae97a7cad73a25cfc3240ab67faeb8
ditchk <blackdollar254@gmail.com>
2024-01-14 17:14:24 +0300

    style update

--- src/components/projects/Featured.tsx
+++ src/components/projects/Featured.tsx
@@ -1,14 +1,13 @@
 import { useGetFeaturedProjects } from "@/lib/Queries/QueriesAndMutations";
 import FeaturedProject from "../shared/FeaturedProject"
 import { Models } from "appwrite";
-import{ IoIosArrowDropleft, IoIosArrowDropright  } from 'react-icons/io'
 
 const Featured = () => {
     const { data: featured } = useGetFeaturedProjects();
   return (
-    <div className="flex flex-row justify-center relative items-center p-1 gap-1">
-      <IoIosArrowDropleft className="slideleft" />
-      <div className="flex w-full h-full justify-center items-center p-10 rounded-lg">
+    <div className="flex flex-col justify-center relative items-center p-1 gap-1">
+      <h2 className="text_title2">Feature project</h2>
+      <div className="flex w-full h-full justify-center items-center p-2 rounded-lg">
         <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {featured?.documents.map((featured: Models.Document) => (
             <li
@@ -19,7 +18,6 @@ const Featured = () => {
             ))}
         </ul>
       </div>
-      <IoIosArrowDropright className="slideleft" />
     </div>
   )
 }
--- src/components/shared/FeaturedProject.tsx
+++ src/components/shared/FeaturedProject.tsx
@@ -14,8 +14,8 @@ const FeaturedProject = ({featured}: FreaturedProjectProps) => {
         to={'/projects'}
         reloadDocument 
       >
-        <img src={featured.Imageurl} alt="" className="object-contain rounded-2xl mb-4 shadow shadow-slate-400" />
-        <h1 className="featuredText text-shadow mb-3">{featured.Title}</h1>
+        <img src={featured.Imageurl} alt="" className="object-contain h-fit w-fit rounded-2xl mb-4 shadow shadow-slate-400" />
+        <h1 className="text_title text-shadow mb-2">{featured.Title}</h1>
         <Button className="custom_button">read more</Button>
       </Link>
     </div>
