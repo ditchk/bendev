117b27e9f0802172a80ab9d9db9ff2595b5bd0a6
ditchk <blackdollar254@gmail.com>
2024-01-14 22:18:08 +0300

    important update

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,4 +1,4 @@
-import Featured from "@/components/projects/Featured"
+// import Featured from "@/components/projects/Featured"
 import Brands from "@/components/shared/Brands"
 import Services from "@/components/shared/Services"
 import Welcome from "@/components/shared/Welcome"
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="default_Container">
       <Welcome />
-      <Featured />
+      {/* <Featured /> */}
       <Brands />
       <Services />
     </div>
