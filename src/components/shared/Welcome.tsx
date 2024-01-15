ab5d1f81644dd7bcbdd53499644236fa552cb754
ditchk <blackdollar254@gmail.com>
2024-01-15 19:14:59 +0300

    important update

--- src/components/shared/Welcome.tsx
+++ src/components/shared/Welcome.tsx
@@ -31,7 +31,7 @@ const Welcome = () => {
                         <h3 className="paragraph_text font-family">I have aquired skills that will help your business scale up in this technology era.Using my expertise i will design stunning landing pages/website and even design your ad campaignsfor your business using the latest technology</h3>
                       </div>
                       <div className="flex flex-row justify-center items-center mt-5 animate-in slide-in-from-bottom-20 duration-700 m-1">
-                          <h2 className="paragraph_text font-family">{link.slogan}<Link className="custom_button p-2 mt-5 rounded-lg text-white font-serif flex" to={'/mycv'}>Continue reading</Link></h2>
+                          <h2 className="paragraph_text font-family">{link.slogan}<Link reloadDocument className="custom_button p-2 mt-5 rounded-lg text-white font-serif flex" to={'/mycv'}>Continue reading</Link></h2>
                           <div className="flex w-14 sm:w-72 justify-center bg-slate-300 h-[2px]" />
                           <div className="flex w-[25px] md:w-[35px] justify-center rounded-full bg-transparent outline outline-2 outline-slate-300 shadow-md shadow-slate-500 h-[20px] md:h-[25px]" />
                         </div>
