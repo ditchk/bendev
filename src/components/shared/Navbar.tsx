e01705815f830172237087996dfd1886a4637925
ditchk <blackdollar254@gmail.com>
2024-01-20 21:44:26 +0300

    test theme

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,7 +5,7 @@ import MobileMenu from './MobileMenu';
 
 const Navbar = () => {
   return (
-    <nav id='navbar' className="z-50 flex flex-row justify-between gap-16 w-full shadow-md bg-cyan-950 bg-opacity-10 backdrop-blur-lg animate-in slide-in-from-top-10 h-max top-0 sticky py-2 px-3 duration-1000">
+    <nav id='navbar' className="z-50 flex flex-row justify-between gap-16 w-full shadow-md bg-cyan-950 border-b border-white bg-opacity-10 backdrop-blur-lg animate-in slide-in-from-top-10 h-max top-0 sticky py-2 px-3 duration-1000">
       <Link 
       to={'/'}
       className="flex flex-row justify-center items-center gap-2"
