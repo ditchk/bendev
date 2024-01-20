bd96e47e88276023ee49e048c73913480cf3f07f
ditchk <blackdollar254@gmail.com>
2024-01-20 22:27:48 +0300

    Confirmed style

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const AuthLayout = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full h-screen">
+    <div className="flex flex-col justify-center items-center px-10 w-full h-screen">
       <nav className="flex justify-start items-start p-2 w-full border-b border-slate-200">
         <Link 
         to={'/'}
@@ -13,7 +13,7 @@ const AuthLayout = () => {
           <h1 className="text-center text-xl font-extrabold font-serif text-transparent bg-clip-text bg-gradient-to-br from-cyan-500 via-black to-cyan-950">BENSWORKSPACE</h1>
         </Link>
       </nav>
-      <div className="flex flex-1 justify-center items-start mt-24 md:mt-0 md:items-center w-full h-fit">
+      <div className="flex flex-1 justify-center items-center w-full h-fit">
         <Outlet />
       </div>
     </div>
