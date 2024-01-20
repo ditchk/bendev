f7bc81a1861107bf0441455843c758e193ab3b9a
ditchk <blackdollar254@gmail.com>
2024-01-20 16:03:41 +0300

    quick fix

--- src/components/home/IconDump.tsx
+++ src/components/home/IconDump.tsx
@@ -16,10 +16,10 @@ const IconDump = () => {
             <CiMicrochip className="absolute text-9xl bg-gradient-to-bl from-violet-500 via-emerald-500 rotate-12 to-red-300 rounded-2xl opacity-10" />
         </div>
         <div>
-            <SiGoogleads  className="absolute text-9xl p-2 -rotate-12 text-white rounded-2xl bg-gradient-to-bl from-violet-500 via-emerald-500 to-red-300 opacity-10" />
+            <SiGoogleads  className="hidden md:flex absolute text-9xl p-2 -rotate-12 text-white rounded-2xl bg-gradient-to-bl from-violet-500 via-emerald-500 to-red-300 opacity-10" />
         </div>
         <div>
-            <SiAdobecreativecloud className="absolute text-9xl p-2 rotate-45 text-white rounded-2xl bg-gradient-to-bl from-violet-500 via-emerald-500 to-red-300 opacity-10" />
+            <SiAdobecreativecloud className="hidden md:flex absolute text-9xl p-2 rotate-45 text-white rounded-2xl bg-gradient-to-bl from-violet-500 via-emerald-500 to-red-300 opacity-10" />
         </div>
         <div>
             <BsGraphUpArrow  className="absolute text-9xl bg-gradient-to-bl from-violet-500 via-emerald-500 rotate-12 to-red-300 rounded-2xl p-1 opacity-10" />
