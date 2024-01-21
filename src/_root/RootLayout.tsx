12c3650d96f4d8b6ea21769184a6996607215ac0
ditchk <blackdollar254@gmail.com>
2024-01-21 10:22:19 +0300

    Confirmed style

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -30,7 +30,7 @@ const RootLayout: React.FC = () => {
       <div className="flex flex-col w-full h-full">
         <Navbar />
         <Sidebar />
-        <section className="flex flex-col">
+        <section className="flex flex-col h-full">
           <Outlet />
         </section>
         <section className="flex">
