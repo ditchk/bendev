3d4c493f9982f0dcbc76b6301ced606989f767a9
ditchk <blackdollar254@gmail.com>
2024-02-05 18:15:12 +0300

    fix upgrades

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -2,12 +2,12 @@ import BannerPost from "@/components/home/BannerPost"
 import IconDump from "@/components/home/IconDump"
 import MobileBanner from "@/components/home/MobileBanner"
 import Services from "@/components/shared/Services"
-import { Slider } from "@/components/sliders/Slider"
+
 
 const Home = () => {
   return (
     <>
-      <Slider />
+      {/* <Slider /> */}
       <IconDump />
       <MobileBanner />
       <BannerPost />
