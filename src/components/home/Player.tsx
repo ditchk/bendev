54b5ce3e2c2e6aff481234e34d7c549cad312c21
ditchk <blackdollar254@gmail.com>
2024-02-10 11:41:12 +0300

    Few changes

--- src/components/home/Player.tsx
+++ src/components/home/Player.tsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import { Button } from '../ui/button';
 
 interface PlayerProps {
-    src: string;
-  }
+  src: string;
+}
 
 const Player: React.FC<PlayerProps> = ({ src }) => {
 
@@ -21,18 +21,18 @@ const Player: React.FC<PlayerProps> = ({ src }) => {
 
   return (
     <div className="Player">
-        <video id="background-video" className="video" autoPlay preload='auto' muted loop>
-            <source src={src} type="video/mp4"/>
-        </video>
-        <div className='flex lg:hidden justify-end mt-72 md:mt-96 bg-white h-8 md:h-10 rounded-t-2xl w-full absolute border-t-2 border-teal-100' />
-        <div className="PlayerItems">
-            <div className="ItemBox-Div">
-                <h1 className="BrandName">WELCOME TO BSWP</h1>
-                <h2 className='BrandName2'>Home for Modern websites</h2>
-                <p className="TextDescription">We strive to make the best of what we have <br />and create something truly special.</p>
-                <Button className="buttonPrev" onClick={handleClick}>Explore Now</Button>
-            </div>
+      <video id="background-video" className="video" autoPlay muted loop>
+        <source src={src} type="video/mp4" />
+      </video>
+      <div className='flex lg:hidden justify-end mt-72 md:mt-96 bg-white h-8 md:h-10 rounded-t-2xl w-full absolute border-t-2 border-teal-100' />
+      <div className="PlayerItems">
+        <div className="ItemBox-Div">
+          <h1 className="BrandName">WELCOME TO BSWP</h1>
+          <h2 className='BrandName2'>Home for Modern websites</h2>
+          <p className="TextDescription">We strive to make the best of what we have <br />and create something truly special.</p>
+          <Button className="buttonPrev" onClick={handleClick}>Explore Now</Button>
         </div>
+      </div>
     </div>
   )
 }
