1577d6ba9a374b8f93f4fff29f425f54894ccf4b
ditchk <blackdollar254@gmail.com>
2024-02-10 08:59:10 +0300

    Few changes

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -9,7 +9,7 @@ import Services from "@/components/shared/Services"
 const Home = () => {
   return (
     <div className="custom_container">
-      <Player src="/assets/videos/welcomevideo.mp4"/> 
+      <Player src={'https://cloud.appwrite.io/v1/storage/buckets/655777900bd1083e6876/files/65c689c641370c99c077/view?project=655773f801147821ba93&mode=admin'} /> 
       <MobileBanner />
       <IconDump />
       <BannerPost />
