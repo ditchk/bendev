1fb9fab55e5f98a175365497e5d32b4d53933ee1
ditchk <blackdollar254@gmail.com>
2024-02-17 09:39:52 +0300

    Few changes

--- src/components/shared/Cards.tsx
+++ src/components/shared/Cards.tsx
@@ -1,11 +1,13 @@
+import { SiAngular, SiFigma, SiReact, SiWebflow, SiWordpress } from "react-icons/si"
+
 const Cards = () => {
   return (
-    <div className="flex flex-row justify-center items-center gap-1">
-      <h1 className="H1Crads">Wordpress</h1>
-      <h1 className="H1Crads">Figma</h1>
-      <h1 className="H1Crads">Webflow</h1>
-      <h1 className="H1Crads">React</h1>
-      <h1 className="H1Crads">Angular</h1>
+    <div className="flex flex-row justify-start items-center gap-1 overflow-x-scroll custom-scrollbar p-5">
+      <h1 className="H1Crads"><SiWordpress className="text-xl md:text-2xl text-blue-600"/>Wordpress </h1>
+      <h1 className="H1Crads"><SiFigma className="text-xl md:text-2xl text-red-600"/>Figma  </h1>
+      <h1 className="H1Crads"><SiWebflow className="text-xl md:text-2xl text-white"/>Webflow </h1>
+      <h1 className="H1Crads"><SiReact className="text-xl md:text-2xl text-cyan-600 animate-spin"/>React </h1>
+      <h1 className="H1Crads"><SiAngular className="text-xl md:text-2xl text-red-900"/>Angular </h1>
     </div>
   )
 }
