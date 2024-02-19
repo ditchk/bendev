6edda8d0e11ba79ca26b14e8e101d979db4a2db1
ditchk <blackdollar254@gmail.com>
2024-02-19 13:37:49 +0300

    Few changes

--- src/components/home/IconDump.tsx
+++ src/components/home/IconDump.tsx
@@ -8,7 +8,7 @@ import { Button } from "../ui/button";
 
 const IconDump = () => {
   return (
-    <section className="flex flex-col w-full h-full my-4">
+    <section className="flex flex-col w-full h-full my-4 mt-8">
       <div className="flex flex-col justify-center items-center gap-4">
       <h1 className="missionText">Crafting Digital Masterpieces, One Pixel at a Time</h1>
             <Button className="custom_button rounded-full animate-bounce">Talk to me now! use the live chat!</Button>
