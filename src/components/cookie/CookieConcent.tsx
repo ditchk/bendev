8ff4a754a7e014f8d16662763d8da2987796a10a
ditchk <blackdollar254@gmail.com>
2024-02-27 19:28:39 +0300

    Few changes

--- src/components/cookie/CookieConcent.tsx
+++ src/components/cookie/CookieConcent.tsx
@@ -8,23 +8,28 @@ const CookieConcent: React.FC = () => {
     setHasAcceptedCookies(true);
   };
 
+  const Declined = () => {
+    setHasAcceptedCookies(false)
+  }
+
   return (
       <CookieConsent
         location="bottom"
         buttonText="I accept"
         declineButtonText="I decline"
         onAccept={handleAcceptCookies}
+        onDecline={Declined}
         acceptOnScroll
         acceptOnScrollPercentage={20}
-        style={{background: "teal", width: "100%", fontSize: "13px", justifyContent: "start", animation: "ease-in-out", boxShadow: "1px 1px 1px teal"}}
-        buttonStyle={{background: "black", color: "white", borderRadius: "10px"}}
+        style={{background: "teal", width: "90%", margin: "15px", fontFamily: "sans-serif", fontWeight: "lighter", left: "0", borderRadius: "20px" ,fontSize: "14px", justifyContent: "start", outline: "4px" , outlineColor: "white" , justifySelf: "end", animation: "ease-in-out"}}
+        buttonStyle={{background: "black", color: "white", borderRadius: "10px", padding: "5px 20px"}}
       >
         This site uses cookies to improve your experience. Learn more in our{" "}
-        <a href="/privacy-policy" style={{color: "black"}}>privacy policy</a>.
+        <a href="/privacy-policy" style={{color: "black", fontFamily: "monospace" , fontWeight: "bolder"}}>privacy policy</a>
         {hasAcceptedCookies ? (
-        <p style={{color: "red"}}>Cookies have been accepted.</p>
+        <small style={{}}></small>
       ) : (
-        <p>Cookies have not been accepted.</p>
+        <small style={{color: "red"}}></small>
       )}
       </CookieConsent>
   );
