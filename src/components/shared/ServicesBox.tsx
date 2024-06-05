78be79fe44f3eb9fd62c134c6d179dc650e4d287
ditchk <blackdollar254@gmail.com>
2024-06-05 09:26:24 +0300

    minor update

--- src/components/shared/ServicesBox.tsx
+++ src/components/shared/ServicesBox.tsx
@@ -10,23 +10,25 @@ const ServicesBox = ({service} : ServiceBoxProps ) => {
 
   return (
     <div className="serviceBox">
-        <div
-            className="titleBox">
-            <h1 className="BoxText">
-                {service.productTitle}
-            </h1>
-        </div>
+        
         <div
             className="ServiceContainer">
         <div className="Image-Cont">
             <img src={service.imageUrl} loading="lazy" alt={"project Management"} width={450} className="ImageUrl-sty"/>
         </div>
-        <Truncate text={service.ProductDescription} maxLength={200} />
+        <div
+            className="DescrIPTBox">
+            <h1 className="BoxText">
+                {service.productTitle}
+            </h1>
+            <Truncate text={service.ProductDescription} maxLength={200} />
+        </div>
+        
         <div className="Button-sect mt-10 mb-2">
-            <Link reloadDocument to={'/packages/basic'} className="BlogLink border-white border-2">
+            <Link reloadDocument to={'/packages/basic'} className="ServLink">
                 <small>Get a quote</small>
             </Link>
-            <Link to={''} className="BlogLink border-white border-2">
+            <Link to={''} className="ServLink bg-teal-800 text-white">
                 <small className="animate-pulse">Starting at 200$</small>
             </Link>
         </div>
