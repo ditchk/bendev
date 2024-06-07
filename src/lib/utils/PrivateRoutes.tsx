568b44cf3e64e6fc5726db357b9d604dd9d9e1a0
ditchk <blackdollar254@gmail.com>
2024-06-07 12:38:00 +0300

    minor update

--- src/lib/utils/PrivateRoutes.tsx
+++ src/lib/utils/PrivateRoutes.tsx
@@ -1,28 +1,17 @@
 import useUserContext from "@/context/useUserContext";
-import { useEffect } from "react";
-import { Outlet, Navigate, useNavigate } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 
 const PrivateRoutes = () => {
     const { isAuthenticated }  = useUserContext()
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        const cookieFallback = localStorage.getItem("cookieFallback");
-        if (
-          cookieFallback === "[]" ||
-          cookieFallback === null ||
-          cookieFallback === undefined
-        ) navigate("/sign-in");
-    
-      }, [navigate]);
-
-    if(!isAuthenticated) {
-        <Navigate to={'/sign-in'} />
-    } else {
+    return (
         <>
-            <Outlet />
+            {isAuthenticated ? (
+                <Outlet />
+            ): (
+                <Navigate to={'/sign-in'} />
+            )}
         </>
-    }
+    )
 }
 
 export default PrivateRoutes
