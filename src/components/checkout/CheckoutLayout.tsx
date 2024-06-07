f6a4cd95f318e91677a635828fde24131b8092d2
ditchk <blackdollar254@gmail.com>
2024-06-07 20:34:35 +0300

    minor update

--- src/components/checkout/CheckoutLayout.tsx
+++ src/components/checkout/CheckoutLayout.tsx
@@ -10,7 +10,7 @@ const CheckoutLayout = () => {
         <div className="hidden lg:flex w-1/2 h-fit overflow-hidden border-r-2 border-slate-300 duration-1000">
             <img src="/assets/images/checkout.jpg" alt="" width={600} />
         </div>
-        <div className="flex flex-col w-full justify-center items-center h-full p-3 mt-4 lg:w-1/2">
+        <div className="flex flex-col w-full justify-center items-start md:items-center h-full p-3 mt-4 lg:w-1/2">
             <Outlet />
         </div>
       </div>
