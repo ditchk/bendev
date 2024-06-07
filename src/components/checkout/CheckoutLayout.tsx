508435772e06699ee097576c63305ed1fd9728f5
ditchk <blackdollar254@gmail.com>
2024-06-07 20:33:03 +0300

    minor update

--- src/components/checkout/CheckoutLayout.tsx
+++ src/components/checkout/CheckoutLayout.tsx
@@ -1,20 +1,20 @@
 import { Outlet } from "react-router"
 import Navbar from "../shared/Navbar"
-import Paymentm from "../payment-methods/Paymentm"
+import Footer from "../shared/Footer"
 
 const CheckoutLayout = () => {
   return (
     <>
     <Navbar />
       <div className="flex flex-row justify-center items-center w-full h-screen">
-        <div className="hidden lg:flex w-1/2 h-fit border-r-2 border-slate-300 duration-1000">
-            <img src="/assets/images/checkout.jpg" alt="" width={700} />
+        <div className="hidden lg:flex w-1/2 h-fit overflow-hidden border-r-2 border-slate-300 duration-1000">
+            <img src="/assets/images/checkout.jpg" alt="" width={600} />
         </div>
-        <div className="flex flex-col w-full justify-center items-center h-full p-3 lg:w-1/2">
+        <div className="flex flex-col w-full justify-center items-center h-full p-3 mt-4 lg:w-1/2">
             <Outlet />
         </div>
       </div>
-      <Paymentm />
+      <Footer />
     </>
   )
 }
