2c236dadfb05384944409ca96967adaf5da79d40
ditchk <blackdollar254@gmail.com>
2024-06-08 17:15:41 +0300

    minor update

--- src/components/home/Player.tsx
+++ src/components/home/Player.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Cards from '../shared/Cards';
 import { Link } from 'react-router-dom';
 import { SiFiverr, SiUpwork } from 'react-icons/si';
+import { FcDribbble } from 'react-icons/fc';
 
 
 interface PlayerProps {
@@ -33,7 +34,7 @@ const Player: React.FC<PlayerProps> = ({ src }) => {
         <div className="ItemBox-Div">
           <h1 className="BrandName">Bswp-Stacks</h1>
           <h2 className='BrandName2'>Home for Modern websites</h2>
-          <p className="TextDescription">We strive to make the best of what we have <br />and create something truly special.</p>
+          <p className="TextDescription">We strive to make the best of what we have and create something truly special.</p>
           <div className='flex flex-row justify-center items-center gap-2'>
             <Link to={'#Choose a services'} className="BlogLink mt-5 z-40 bg-transparent text-teal-500 outline-teal-500" onClick={handleClick}>Get Started</Link>
             <Link to={'/blogs'} reloadDocument className="BlogLink mt-5 z-40 outline-white text-white bg-teal-950">Blog Library</Link>
@@ -49,10 +50,13 @@ const Player: React.FC<PlayerProps> = ({ src }) => {
           <h3 className="HireTextCard">Hire me now through:</h3>
           <div className="flex flex-row gap-3">
             <Link to={'https://www.upwork.com/fl/bensonk31?s=1017484851352698949'} className="flex flex-row text-xl font-bold text-green-500 font-moon-dance justify-center items-center">
-              <SiUpwork className="text-green-500"/>work
+              <SiUpwork className="text-green-500 text-3xl"/>work
             </Link>
             <Link to={'https://www.fiverr.com/s/bddGlGk'} className="flex flex-row justify-center items-center">
               <SiFiverr className="text-green-700 text-5xl"/>
+            </Link>
+            <Link to={'https://www.fiverr.com/s/bddGlGk'} className="flex flex-row font-moon-dance text-pink-600 text-xl font-bold justify-center items-center">
+              <FcDribbble className="text-green-700 text-3xl"/>ribble
             </Link>
           </div>
         </div>
