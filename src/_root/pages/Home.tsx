ddc76f53543680da61c1a7c6456980339ef25aa8
ditchk <blackdollar254@gmail.com>
2024-06-10 12:53:13 +0300

    minor update

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,19 +1,46 @@
 import BannerPost from "@/components/home/BannerPost"
 import MobileBanner from "@/components/home/MobileBanner"
-import MotionIcons from "@/components/home/MotionIcons"
 import PackagesSect from "@/components/home/PackagesSect"
 import Player from "@/components/home/Player"
-import ServiceBox from "@/components/shared/Services"
+import ServiceLoader from "@/components/shared/ServiceLoader"
+import ServicesBox from "@/components/shared/ServicesBox"
+import { useGetAllServices } from "@/lib/Queries/QueriesAndMutations"
+import { Models } from "appwrite"
 
 const Home = () => {
+
+  const { data: products, isPending: isLoading } = useGetAllServices();
+
   return (
     <div className="custom_container">
-      <Player src={'https://cloud.appwrite.io/v1/storage/buckets/655777900bd1083e6876/files/665f3bf2007954b2e2/view?project=655773f801147821ba93&mode=admin'} />
-      <MobileBanner />
-      <MotionIcons />
-      <BannerPost />
-      <PackagesSect />
-      <ServiceBox />
+      <div className="PlayerBox">
+        <Player src={'https://cloud.appwrite.io/v1/storage/buckets/655777900bd1083e6876/files/665f3bf2007954b2e2/view?project=655773f801147821ba93&mode=admin'} />
+      </div>
+      <div className="BannerBox">
+        <MobileBanner />
+      </div>
+      <div className="BannerBox">
+        <BannerPost />
+      </div>
+      <div className="PackageBox">
+        <PackagesSect />
+      </div>
+      <div className="serviceBox">
+      <div className="MainServiceBox" id="Choose a services">
+      <h1 className="PackText font-bold mb-4">Choose a service</h1>
+      {isLoading && !products ? (
+        <ServiceLoader />
+        ) : (
+          <div className="Box-listify">
+            {products?.documents.map((service: Models.Document) => (
+              <div key={service.$id} className="ServiceList">
+                <ServicesBox service={service} key={service.productTitle} />
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+      </div>
     </div>
   )
 }
